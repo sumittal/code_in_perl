802f1ce66d543ae3d67003cbd3f5d216c8ca5cdb
sumittal <mittal.sumit1984@gmail.com>
2015-09-29 14:49:10 +0530

    Detect the Email Addresses

---
+++ string_7_email_address.pl
@@ -0,0 +1,25 @@
+use v5.14;
+use warnings;
+
+my $nol = <> + 0;
+
+my $r = '\b[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}\b';
+my $regex = quotemeta($r);
+my %mail;
+my @final;
+
+foreach (1 .. $nol) {
+    my $line = <>;
+    chomp($line);
+
+    if($line =~ /($r)/){
+        $mail{$1} += 1;
+    }
+}
+
+foreach my $key (sort {$a cmp $b} keys %mail) {
+    push @final, $key;
+}
+
+my $output = join(';', @final);
+say $output;
