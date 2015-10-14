65678435bce50a40466ecf8c8914bd25c3aeb039
Sumit Mittal <mittal.sumit1984@gmail.com>
2015-10-14 09:45:49 +0530

    funny_string.pl

--- String/funny_string.pl
+++ String/funny_string.pl
@@ -30,3 +30,32 @@ sub is_funny {
     }
     $flag ? say 'Funny' : say 'Not Funny';
 }
+
+__DATA__
+Problem Statement
+
+Suppose you have a string S which has length N and is indexed from 0 to N−1. String R is the reverse of the string S. The string S is funny if the condition |Si−Si−1|=|Ri−Ri−1| is true for every i from 1 to N−1.
+
+(Note: Given a string str, stri denotes the ascii value of the ith character (0-indexed) of str. |x| denotes the absolute value of an integer x)
+
+Input Format
+
+First line of the input will contain an integer T. T testcases follow. Each of the next T lines contains one string S.
+
+Constraints
+
+1<=T<=10
+2<=length of S<=10000
+Output Format
+
+For each string, print Funny or Not Funny in separate lines.
+
+Sample Input
+
+2
+acxz
+bcxz
+Sample Output
+
+Funny
+Not Funny
